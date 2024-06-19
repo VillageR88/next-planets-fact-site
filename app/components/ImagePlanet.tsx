c813a67182c4cb558bffb093b27d59780aef692e
VillageR88 <villager88pl@gmail.com>
2024-06-19 20:01:53 +0200

    improved layout mobile
    Signed-off-by: VillageR88 <villager88pl@gmail.com>

--- app/components/ImagePlanet.tsx
+++ app/components/ImagePlanet.tsx
@@ -1,5 +1,5 @@
 import Image from 'next/image';
 
 export default function ImagePlanet({ src, alt }: { src: string; alt: string }) {
-  return <Image fill src={src} className="size-full" alt={alt} />;
+  return <Image priority fill src={src} className="size-full" alt={alt} />;
 }
