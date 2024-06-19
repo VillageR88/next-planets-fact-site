5b2c0ff8c567ac6ab1ef5304ba440e094e134f1b
VillageR88 <villager88pl@gmail.com>
2024-06-19 18:26:51 +0200

    improved functionality
    Signed-off-by: VillageR88 <villager88pl@gmail.com>

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,6 +5,7 @@ import Image from 'next/image';
 import backgroundImage from '@/public/assets/starter-code/assets/background-stars.svg';
 import DataProvider from './_lib/DataContext';
 import Navbar from './home/navbar';
+import ImagePlanet from './components/ImagePlanet';
 
 const antonio = Antonio({
   display: 'swap',
@@ -38,11 +39,19 @@ export default function RootLayout() {
       <body
         className={`${antonio.variable} ${leagueSpartan.variable} mx-auto w-full max-w-[90em] overflow-x-clip bg-[#070724]`}
       >
-        <Image className="absolute -z-10 object-cover" fill src={backgroundImage as string} alt="background image" />
+        <Image
+          className="absolute left-0 top-0 -z-10 size-full object-cover"
+          width={10000}
+          height={1}
+          src={backgroundImage as string}
+          alt="background image"
+        />
         <Navbar />
         <DataProvider>
           <div className="mx-auto flex max-w-[1110px] justify-between pl-[170px]">
-            <div className="mt-[242px] size-[290px] bg-white"></div>
+            <div className="mt-[242px] size-[290px] bg-white">
+              <ImagePlanet />
+            </div>
             <div className="mt-[126px] h-[541px] w-[350px] bg-white"></div>
           </div>
           <ul className="mx-auto mt-[87px] flex w-full max-w-[1110px] justify-between gap-[30px]">
